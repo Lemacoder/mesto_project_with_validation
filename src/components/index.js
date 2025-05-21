e39ed938ac10a790ae6e78b9b56c122cd51cccae
Lemacoder <ladygin.anastasia@yandex.ru>
2025-05-21 14:00:23 +0300

    test

---
+++ src/components/index.js
@@ -0,0 +1,35 @@
+import { openImagePopup } from './imagePopup.js';
+import { editProfile } from './profile.js';
+import { enableValidation } from './validForm.js';
+import { serverUser, serverCards, addLike, deleteLike, deleteCard } from './server.js';
+import '../pages/index.css';
+
+
+export const placesList = document.querySelector('.places__list');
+
+placesList.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('card__like-button')) {
+        
+        if (evt.target.classList.contains('card__like-button_is-active')) {
+            evt.target.classList.remove('card__like-button_is-active');
+            deleteLike(evt.target.parentElement.parentElement.querySelector('.card__id').textContent);
+        } else {
+            evt.target.classList.add('card__like-button_is-active');
+            addLike(evt.target.parentElement.parentElement.querySelector('.card__id').textContent);
+            
+        }
+        
+    }
+    if (evt.target.classList.contains('card__delete-button')) {
+        evt.target.closest('.places__item').remove();
+        deleteCard(evt.target.parentElement.querySelector('.card__id').textContent);
+    }
+    if (evt.target.classList.contains('card__image')) {
+        openImagePopup(evt.target.alt, evt.target.src);
+    }
+});
+
+editProfile();
+enableValidation();
+serverUser();
+serverCards();
---
+++ src/components/server.js
@@ -0,0 +1,103 @@
+import { placesList } from './index';
+import { createCard } from './card.js';
+
+let user_id = null;
+
+export function serverUser() {
+    fetch('https://nomoreparties.co/v1/apf-cohort-202/users/me', {
+        headers: {
+          authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6'
+        }
+      })
+        .then(res => res.json())
+        .then((result) => {
+            document.querySelector('.profile__image').style.backgroundImage = result.avatar;
+            document.querySelector('.profile__title').textContent = result.name;
+            document.querySelector('.profile__description').textContent = result.about;
+            user_id = result._id;
+    });
+}
+
+export function serverCards() {
+    fetch('https://nomoreparties.co/v1/apf-cohort-202/cards', {
+        headers: {
+          authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6'
+        }
+      })
+        .then(res => res.json())
+        .then((result) => {
+            for (let i=0; i < result.length; i++) {
+              if (result[i].owner._id !== user_id) {
+                placesList.append(createCard(result[i].name, result[i].link, result[i].likes.length, false, result[i]._id));
+              } else {
+                placesList.append(createCard(result[i].name, result[i].link, result[i].likes.length, true, result[i]._id));
+              }
+            }
+    });
+}
+
+export function updateUser(Name, Description) {
+  fetch('https://nomoreparties.co/v1/apf-cohort-202/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: Name,
+      about: Description
+    })
+  }); 
+}
+
+export function appendCard(CardName, TypeUrl) {
+  return fetch('https://nomoreparties.co/v1/apf-cohort-202/cards', {
+    method: 'POST',
+    headers: {
+      authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: CardName,
+      link: TypeUrl
+    })
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    return data._id;
+  });
+}
+
+export function deleteCard (cardId) {
+  const requst = 'https://nomoreparties.co/v1/apf-cohort-202/cards/' + cardId;
+  fetch(requst, {
+    method: 'DELETE',
+    headers: {
+      authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6',
+    },
+  });
+}
+
+export function addLike (cardId) {
+  const requst = 'https://nomoreparties.co/v1/apf-cohort-202/cards/likes/' + cardId;
+  fetch(requst, {
+    method: 'PUT',
+    headers: {
+      authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6',
+    },
+  });
+
+  
+}
+
+export function deleteLike (cardId) {
+  const requst = 'https://nomoreparties.co/v1/apf-cohort-202/cards/likes/' + cardId;
+  fetch(requst, {
+    method: 'DELETE',
+    headers: {
+      authorization: 'c6f39b35-acde-4bea-9a76-beb0de6a82d6',
+    },
+  });
+}
